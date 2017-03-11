b731a04e8942f5e6f05ade00bfb8d29688d2a851
Mark <usertzc@gmail.com>
2017-03-11 13:55:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 directory="/etc/ntp.conf"
 if [ "$(ls $directory)" ]; then
-	/usr/bin/ntpd -c /etc/ntp.conf -p /tmp/ntpd.pid -d
+	/usr/sbin/ntpd -c /etc/ntp.conf -p /tmp/ntpd.pid -d
 else
 	curl -Lks4 https://raw.githubusercontent.com/LinuxEA-Mark/docker-ntp/master/ntp.conf -o /etc/ntp.conf
 fi
